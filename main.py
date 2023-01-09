552d34278ea9b419a912e517e921be7f690d0adf
Hassan Fares <abdallahffh@gmail.com>
2023-01-09 00:10:12 -0400

    Finish module 3 and start module 4 and 5

--- main.py
+++ main.py
@@ -20,9 +20,21 @@ class Estructura():
             elif orden[:5] == "rmdir": #**********RMDIR*********
                 self.rmDir(orden[6:])
             elif orden[:6] == "rename": #**********RENAME*********
-                self.rename(orden.split(" ")[1], orden.split(" ")[2])
+                try:
+                    self.rename(orden.split(" ")[1], orden.split(" ")[2])
+                except IndexError:
+                    print("Por favor, indique el nombre viejo y el nuevo.")
+                    
+            elif orden[:6] == "create": #**********CREATE*********
+                self.create(orden[7:])
+            elif orden[:6] == "remove": #**********REMOVE*********:
+                self.remove(orden[7:])
+            elif orden[:4] == "open": #**********OPEN*********:
+                self.open(orden[5:])
             else:
                 print('"'+ orden + '" ' + 'no se reconoce como un comando interno o externo, programa o archivo por lotes ejecutable. \n')
+
+    #****************DIRECTORIOS*************
                 
     def changeDir(self, orden):
         nombre = orden[1:]
@@ -58,10 +70,62 @@ class Estructura():
                 print('No existe ningun directorio con el nombre "' + dir + '"')
     
     def rename(self, old, new):
+        if self.currentDir.directorios == []:
+            print('No existe ningun directorio con el nombre "' + old + '"')
+
         for i in self.currentDir.directorios:
             if old == i.nombre:
                 setattr(i, "nombre", new)
                 setattr(i, "path", self.path + "\\" + new)
+            else:
+                print('No existe ningun directorio con el nombre "' + old + '"')
+                
+    #****************DIRECTORIOS*************
+    #****************FICHEROS****************
+    def create(self, args): # PERMITE CREAR Y AGREGAR TEXTO DIRECTAMENTE O SIMPLEMENTE CREAR
+        if len(args.split(" ")) == 1:
+            try:
+                self.currentDir.directorios.append(Fichero(args, args.split(".")[1]))
+            except IndexError:
+                print("Por favor indique una extension del archivo")
+        elif len(args.split(" ")) > 1:
+            self.currentDir.directorios.append(Fichero(args.split(" ")[0], args.split(" ")[0].split(".")[1]))
+            setattr(self.currentDir.directorios[-1], "contenido", args.split(" ", 1)[1])
+            
+    def remove(self, arg):
+        if arg == "":
+            print("Por favor, indique el nombre del archivo")
+        else:
+            for i in self.currentDir.directorios:
+                if i.tipo != "<DIR>":
+                    if arg == i.nombre:
+                        self.currentDir.directorios.remove(i)
+                    else:
+                        print('No existe ningun fichero con el nombre "' + arg + '"')
+                        
+    def open(self, arg):
+        if arg == "":
+            print("Por favor, indique el nombre del archivo")
+        else:
+            for i in self.currentDir.directorios:
+                if i.tipo != "<DIR>":
+                    if arg == i.nombre:
+                        print(i.contenido)
+                        input()
+                    else:
+                        print("No se ha encontrado el archivo. Por favor, vuelva a intentarlo.")
+                else:
+                    print("No se ha encontrado el archivo. Por favor, vuelva a intentarlo.")
+
+    
+    
+    
+    
+    
+    
+    #****************FICHEROS****************
+
+        
 
 
 
@@ -74,6 +138,13 @@ class Directorio():
         self.tipo = "<DIR>"
         self.directorios = []
         
+class Fichero():
+    def __init__(self, nombre, tipo) -> None:
+        self.nombre = nombre
+        self.tipo = tipo # Extension
+        self.contenido = ""
+        self.fCreacion = datetime.datetime.now().strftime("%x")
+        self.hCreacion = datetime.datetime.now().strftime("%X")
         
-print(datetime.datetime.now().strftime("%x"))
+        
 main = Estructura()
